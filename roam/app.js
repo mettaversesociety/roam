be70653aa14fd60651e4e06435d1101233cc910a
Danilo Chiarlone <danilochiarlone@hotmail.com>
2021-02-21 13:05:04 +0000

    Merged PR 8: attempt at multiplayer
    Related work items: #11

--- roam/app.js
+++ roam/app.js
@@ -24,7 +24,7 @@ var io = socket(server);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,15 +35,66 @@ app.use('/', routes);
 app.use('/users', users);
 
 // socketIO stuff
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.sockets.on('connection', (socket) => {
+    socket.userData = { x: 0, y: 0, z: 0, header: 0 };
+
+    console.log(`${socket.id} connected`);
+    socket.emit('setId', { id: socket.id });
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log(`Player ${socket.id} disconnected`);
+        socket.broadcast.emit('deletePlayer', { id: socket.id });
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+
+    socket.on('init', (data) => {
+        console.log(`socket.init ${data.model}`);
+        socket.userData.model = data.model;
+        socket.userData.colour = data.colour;
+        socket.userData.x = data.x;
+        socket.userData.y = data.y;
+        socket.userData.z = data.z;
+        socket.userData.headering = data.h;
+        socket.userData.pb = data.pb;
+        socket.userData.action = "Idle";
+    });
+
+    socket.on('update', (data) => {
+        socket.userData.x = data.x;
+        socket.userData.y = data.y;
+        socket.userData.z = data.z;
+        socket.userData.headering = data.h;
+        socket.userData.pb = data.pb;
+        socket.userData.action = data.action;
+    });
+
+    socket.on('chat message', (data) => {
+        console.log(`chat message: ${data.id} ${data.message}`);
+        io.to(data.id).emit('chat message', { id: socket.id, message: data.message });
     });
 });
+
+setInterval(function () {
+    const nsp = io.of('/');
+    let pack = [];
+    for (let id in io.sockets.sockets) {
+        const socket = nsp.connected[id];
+        // Only push sockets that have been initialized
+        if (socket.userData.model !== undefined) {
+            pack.push({
+                id: socket.id,
+                model: socket.userData.model,
+                colour: socket.userData.colour,
+                x: socket.userData.x,
+                y: socket.userData.y,
+                z: socket.userData.z,
+                header: socket.userData.heading,
+                pb: socket.userData.pb,
+                action: socket.userData.action
+            });
+        }
+    }
+    if (pack.length > 0) io.emit('remoteData', pack);
+}, 40); // 25 times a second -- 40 ms between each interval 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
